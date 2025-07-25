a580ff7bcdbe939adccca25eabaa6c40ce16c7fb
Jay Paygude <jaypaygude2@gmail.com>
2025-07-25 15:09:14 +0530

    Create size_of.cpp

---
+++ size_of.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Data Type Sizes (in bytes):" << endl;
+    cout << "int: " << sizeof(int) << endl;
+    cout << "float: " << sizeof(float) << endl;
+    cout << "bool: " << sizeof(bool) << endl;
+    cout << "long int: " << sizeof(long int) << endl;
+    cout << "short int: " << sizeof(short int) << endl;
+    cout << "double: " << sizeof(double) << endl;
+    cout << "char: " << sizeof(char) << endl;
+    return 0;
+}
